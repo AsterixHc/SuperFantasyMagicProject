21ae8176651edca3a34210994e4e0edf7658190e
Mikkel Nielsen-Man <pallevader@gmail.com>
2019-12-02 14:17:13 +0100

    Enemy positions in BattleScreen

--- SuperFantasyMagicProject/SuperFantasyMagicProject/Screen/BattleScreen.cs
+++ SuperFantasyMagicProject/SuperFantasyMagicProject/Screen/BattleScreen.cs
@@ -16,9 +16,13 @@ namespace SuperFantasyMagicProject.Screen
 
         //Background image for the splash screen.
         private Texture2D background;
-        private Texture2D enemy0Sprite;
-        private Texture2D enemy1Sprite;
-        private Texture2D enemy2Sprite;
+        private Texture2D enemy0Sprite, enemy1Sprite, enemy2Sprite;
+
+        //Positions for screen elements (players, enemies)
+        Vector2 enemy0Position = new Vector2(1610, 160);
+        Vector2 enemy1Position = new Vector2(1610, 400);
+        Vector2 enemy2Position = new Vector2(1610, 640);
+        Vector2 player0Position = new Vector2(0,0);
 
         //Path to the background image.
         private string path = "BattleScreen/Background";
@@ -45,9 +49,9 @@ namespace SuperFantasyMagicProject.Screen
             enemies[0] = enemy0;
             enemies[1] = enemy1;
             enemies[2] = enemy2;
-            enemies[0].Position = new Vector2(1610, 160);
-            enemies[1].Position = new Vector2(1610, 400);
-            enemies[2].Position = new Vector2(1610, 640);
+            enemies[0].Position = enemy0Position;
+            enemies[1].Position = enemy1Position;
+            enemies[2].Position = enemy2Position;
         }
 
         public override void LoadContent()
