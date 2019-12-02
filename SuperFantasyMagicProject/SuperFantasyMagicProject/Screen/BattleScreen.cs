c25a2d0423e72f0fa20ec4f669d12c251ad6c868
Daniel Justesen <Daniel@justesen.biz>
2019-12-02 01:15:58 +0100

    Start
    Started to Transfer Files from old GitHub to New GitHub

---
+++ SuperFantasyMagicProject/SuperFantasyMagicProject/Screen/BattleScreen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SuperFantasyMagicProject.Screen
+{
+    class BattleScreen : GameScreen
+    {
+
+        private int expValue;
+        public int ExpValue { get => expValue; }
+        //Background image for the splash screen.
+        private Texture2D background;
+        //Path to the background image.
+        private string path = "BattleScreen/Background";
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public BattleScreen()
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            background = gameScreenContent.Load<Texture2D>(path);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+        }
+    }
+}
---
+++ SuperFantasyMagicProject/SuperFantasyMagicProject/ScreenManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using SuperFantasyMagicProject.Screen;
+
+namespace SuperFantasyMagicProject
+{
+    static class ScreenManager
+    {
+        //ContentManager for handling all content in the game.
+        private static ContentManager contentManager;
+
+        //Dimensions of the game window (Width, height).
+        private static Vector2 screenDimensions = new Vector2(1280, 720);
+
+        //The GameScreen that is currently being displayed.
+        private static GameScreen currentScreen;
+
+        public static ContentManager ContentManager { get => contentManager; private set => contentManager = value; }
+        public static Vector2 ScreenDimensions { get => screenDimensions; private set => screenDimensions = value; }
+
+        /// <summary>
+        /// Sets initial screen parameters when game starts.
+        /// </summary>
+        public static void Initialize()
+        {
+            currentScreen = new BattleScreen();
+        }
+
+        /// <summary>
+        /// Loads content required by the current game screen.
+        /// </summary>
+        /// <param name="contentManager"></param>
+        public static void LoadContent(ContentManager contentManager)
+        {
+            ContentManager = new ContentManager(contentManager.ServiceProvider, "Content");
+            currentScreen.LoadContent();
+        }
+
+        /// <summary>
+        /// Unloads content no longer needed by the game whenever current screen changes.
+        /// </summary>
+        public static void UnloadContent()
+        {
+
+        }
+
+        /// <summary>
+        /// Updates game elements on the screen.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public static void Update(GameTime gameTime)
+        {
+            currentScreen.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Draws the screen.
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            currentScreen.Draw(spriteBatch);
+        }
+
+
+    }
+}
