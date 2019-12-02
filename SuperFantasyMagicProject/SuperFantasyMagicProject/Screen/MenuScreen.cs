db327f181ce13db95f5463e49429f3be15e314f5
Daniel Justesen <Daniel@justesen.biz>
2019-12-02 01:22:26 +0100

    Screen
    Finished Transfering all the Screen Scrips

---
+++ SuperFantasyMagicProject/SuperFantasyMagicProject/Screen/MenuScreen.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SuperFantasyMagicProject.Screen
+{
+    /// <summary>
+    /// Button for Play
+    /// Button for Pause
+    /// Button for UnPause
+    /// Button for Exit
+    /// </summary>
+    class MenuScreen : GameScreen
+    {
+        //Background image for the menu screen.
+        private Texture2D background;
+        //Path to the background image.
+        private string path = "MenuScreen/Background";
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public MenuScreen()
+        {
+
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            background = gameScreenContent.Load<Texture2D>(path);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+        }
+    }
+}
