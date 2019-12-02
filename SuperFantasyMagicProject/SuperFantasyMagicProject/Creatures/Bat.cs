1352eb5e151790a0082bead2f247528c66b8d3c6
Daniel Justesen <Daniel@justesen.biz>
2019-12-02 01:41:47 +0100

    Creatures
    Added all the Creatures/Character Sub Classes to the Creatures Folder

---
+++ SuperFantasyMagicProject/SuperFantasyMagicProject/Creatures/Bat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperFantasyMagicProject.Creatures
+{
+    class Bat : Characters
+    {
+
+        public override void Attack()
+        {
+            //Attack at random against Player
+        }
+
+        public override void SpecialAttack()
+        {
+            //Applies Screatch against Player
+            //Reduce Player Strength and Health
+        }
+
+        public override bool TakeDamage(int dmg)
+        {
+            //Reduce currentHealth by damage amount
+            throw new NotImplementedException();
+        }
+    }
+}
