4aa3b6928929cc4316fe309f8c2d414b9bd20f7e
Magnus Poulsen <magnux661@gmail.com>
2019-12-09 23:30:14 +0100

    GameoverScreen button attempt
    Skærm færdig, men stadig ikke nogle knapper

---
+++ SuperFantasyMagicProject/SuperFantasyMagicProject/GameOver/GameOverButton.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperFantasyMagicProject.Creatures;
+using SuperFantasyMagicProject.GameOver;
+
+namespace SuperFantasyMagicProject
+{
+    class GameOverButton : Componet
+    {
+        private MouseState previousMS = Mouse.GetState();
+        private MouseState currentMS;
+        private bool isHovering;
+        public SpriteFont font1;
+
+        private Texture2D texture1;
+
+        public Color PenColour { get; set; }
+        public Vector2 Position { get; set; }
+        public string Text { get; set; }
+        public EventHandler Click;
+
+        public Rectangle rectangle
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, texture1.Width, texture1.Height);
+            }
+        }
+
+        public GameOverButton(Texture2D texture, SpriteFont font)
+        {
+            texture1 = texture;
+            font1 = font;
+            PenColour = Color.White;
+        }
+
+        public void LoadContent()
+        {
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            var colour = Color.White;
+
+            if (isHovering)
+            {
+                colour = Color.Gray;
+            }
+
+            spriteBatch.Draw(texture1, rectangle, colour);
+
+            if (!string.IsNullOrEmpty(Text))
+            {
+                var x = (rectangle.X + (rectangle.Width / 2)) - (font1.MeasureString(Text).X / 2);
+                var y = (rectangle.Y + (rectangle.Height / 2)) - (font1.MeasureString(Text).Y / 2);
+
+                spriteBatch.DrawString(font1, Text, new Vector2(x, y), PenColour);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            currentMS = Mouse.GetState();
+            var mouseRectangle = new Rectangle(currentMS.X, currentMS.Y, 1, 1);
+
+            isHovering = false;
+
+            if (mouseRectangle.Intersects(rectangle))
+            {
+                isHovering = true;
+                if (currentMS.LeftButton == ButtonState.Released && previousMS.LeftButton == ButtonState.Pressed)
+                {
+                    Click?.Invoke(this, new EventArgs());
+                }
+            }
+        }
+    }
+}
--- SuperFantasyMagicProject/SuperFantasyMagicProject/Screen/GameOverScreen.cs
+++ SuperFantasyMagicProject/SuperFantasyMagicProject/Screen/GameOverScreen.cs
@@ -6,126 +6,44 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using SuperFantasyMagicProject.Creatures;
 
 namespace SuperFantasyMagicProject.Screen
 {
     class GameOverScreen : GameScreen
     {
-        private MouseState previousMS = Mouse.GetState();
-        private MouseState newMS;
-
+        
         private Texture2D background;
-        private Texture2D texture, respawnInactive, respawnActive, respawnDown;
-        private Texture2D texture2, quitInactive, quitActive, quitDown;
         private string backgroundPath = "GameOverScreen/gameover1";
-        private string respawnInactivePath = "GameOverScreen/Respawn1";
-        private string respawnActivePath = "GameOverScreen/Respawn2";
-        private string respawnDownPath = "GameOverScreen/Respawn3";
-        private string quitInactivePath = "GameOverScreen/Quit1";
-        private string quitActivePath = "GameOverScreen/Quit2";
-        private string quitDownPath = "GameOverScreen/Quit3";
-
-        private Rectangle collisionBox;
-        private Vector2 origin;
-
-        private bool isDown;
-        private Vector2 firstELeftButton;
-        public Vector2 Position { get; set; }
-        public Vector2 Position2 { get; set; }
-        public bool Activated { get; private set; }
 
         public GameOverScreen()
         {
-            Position = Vector2.Zero;
-            Position2 = Vector2.Zero;
-            isDown = false;
-            Activated = false;
+            
         }
-
 
         public override void LoadContent()
         {
             base.LoadContent();
             background = gameScreenContent.Load<Texture2D>(backgroundPath);
-            respawnInactive = MenuManager.Content.Load<Texture2D>(respawnInactivePath);
-            respawnActive = MenuManager.Content.Load<Texture2D>(respawnActivePath);
-            respawnDown = MenuManager.Content.Load<Texture2D>(respawnDownPath);
-            quitInactive = MenuManager.Content.Load<Texture2D>(quitInactivePath);
-            quitActive = MenuManager.Content.Load<Texture2D>(quitActivePath);
-            quitDown = MenuManager.Content.Load<Texture2D>(quitDownPath);
-            texture = respawnInactive;
-            texture2 = quitInactive;
-            origin=new Vector2(texture.Height / 2, texture.Width / 2);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            HandleInput();
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
-            spriteBatch.Draw(texture, Position, null, Color.White, 0, origin, 1, SpriteEffects.None, 1f);
-            spriteBatch.Draw(texture2, Position2, null, Color.White, 0, origin, 1, SpriteEffects.None, 1f);
         }
-
-        public void Update()
-        {
-            HandleInput();
-        }
-
         public override void HandleInput()
         {
-            newMS = Mouse.GetState();
-            Rectangle mouseRectangle = new Rectangle(newMS.X, newMS.Y, 1, 1);
-
-            if (mouseRectangle.Intersects(collisionBox))
-            {
-                //Click event, button is up
-                if (newMS.LeftButton == ButtonState.Pressed && previousMS.LeftButton == ButtonState.Released && !isDown)
-                {
-                    isDown = true;
-                }
-                //Release event, button is down
-                else if (newMS.LeftButton == ButtonState.Released && previousMS.LeftButton == ButtonState.Pressed && isDown)
-                {
-                    isDown = false;
-                    Activated = true;
-                }
-
-                if (!isDown && texture != quitActive)
-                {
-                    texture = quitActive;
-                }
-                else if (isDown && texture != quitDown)
-                {
-                    texture = quitDown;
-                }
-                if (!isDown && texture != respawnActive)
-                {
-                    texture = respawnActive;
-                }
-                else if (isDown && texture != respawnDown)
-                {
-                    texture = respawnDown;
-                }
-            }
-            else
-            {
-                texture = quitInactive;
-
-                //Release event, button is down
-                if (newMS.LeftButton == ButtonState.Released && previousMS.LeftButton == ButtonState.Pressed && isDown)
-                {
-                    isDown = false;
-                }
-
-                texture = respawnInactive;
-
-                if (newMS.LeftButton == ButtonState.Released && previousMS.LeftButton == ButtonState.Pressed && isDown)
-                {
-                    isDown = false;
-                }
-            }
-
-            previousMS = newMS;
+            
         }
     }
 }
